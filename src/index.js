1561af2590c582a5638fbc15a2ca88ff513a9c6b
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-07-20 17:33:17 +0100

    data destructuring

--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
 // index.js
 
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 import {Header} from './header'
 
 // class based component
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const data = {
+    welcome: 'Welcome to 30 Days Of React',
+    title: 'Getting Started React',
+    subtitle: 'JavaScript Library',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+    date: 'Oct 6, 2020',
   }
-  render() {
-    const data = {
-      welcome: "Welcome to 30 Days Of React",
-      title: "Getting Started React",
-      subtitle: "JavaScript Library",
-      author: {
-        firstName: "Asabeneh",
-        lastName: "Yetayeh",
-      },
-      date: "Oct 7, 2020",
-    };
 
-    return (
-      <div className="app">
-        <Header data={data} />
-      </div>
-    );
-  }
+  return (
+    <div className='app'>
+      <Header data={data} />
+    </div>
+  )
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
