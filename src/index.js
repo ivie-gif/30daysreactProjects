fed39417d2d8eee338435e780068b88784947874
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-07-21 18:02:03 +0100

    re-structure the code with separate methods

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ class App extends React.Component {
     count: 1,
   };
 
+  addValue = () => {
+    this.setState({count: this.state.count + 1})
+  };
+
+  minusValue = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
   render() {
     return (
       <div>
@@ -15,8 +23,8 @@ class App extends React.Component {
         </div>
 
         <div style={{ display: "flex"}}>
-          <button onClick={() => this.setState({count: this.state.count + 1})}>Increase Value</button>
-          <button style={{marginLeft: '10px'}} onClick={() => this.setState({count: this.state.count - 1})}>Decrease Value</button>
+          <button onClick={this.addValue}>Increase Value</button>
+          <button style={{marginLeft: '10px'}} onClick={this.minusValue}>Decrease Value</button>
         </div>
       </div>
     );
