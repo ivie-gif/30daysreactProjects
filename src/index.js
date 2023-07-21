3c9cc564df7d86e7486b26bd1b3cf857a50d2cec
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-07-21 17:55:41 +0100

    Resetting a state using a JavaScript method plus and minus

--- src/index.js
+++ src/index.js
@@ -1,41 +1,27 @@
-// index.js
-
-// Class Component
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  // declaring state
+  // Setting our initial state
   state = {
-    count: 5,
-  }
+    count: 1,
+  };
+
   render() {
-    // accessing the state value
-    // const count = this.state.count
     return (
-      <div className='App'>
-        <h1>{this.state.count} </h1>
-        <button onClick={() => this.setState({count: this.state.count + 1})}>Click Here</button>
+      <div>
+        <div className="App">
+          <h1>{this.state.count}</h1>
+        </div>
+
+        <div style={{ display: "flex"}}>
+          <button onClick={() => this.setState({count: this.state.count + 1})}>Increase Value</button>
+          <button style={{marginLeft: '10px'}} onClick={() => this.setState({count: this.state.count - 1})}>Decrease Value</button>
+        </div>
       </div>
-    )
+    );
   }
 }
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
 
-
-// import React, { useState } from 'react'
-// import ReactDOM from 'react-dom'
-
-// Functional component 
-// const App = () => {
-//   const [count, setCount] = useState(2)
-
-//   return (
-//     <div className='App'>
-//       <h1>{count}</h1>
-//       <button onClick={()=> setCount(count + 1)}> Increase value</button>
-//     </div>
-//   )
-// }
-
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
