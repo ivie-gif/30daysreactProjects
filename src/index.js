0db100f09967349488ce60e8d53d07b8a08167f7
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-08-02 22:46:59 +0100

    using state class component

--- src/index.js
+++ src/index.js
@@ -2,37 +2,25 @@
 import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
-// class App extends React.Component {
-//   // declaring state
-//   state = {
-//     count: 0,
-//   }
-//   render() {
-//     // accessing the state value
-//     const count = this.state.count
-//     return (
-//       <div className='App'>
-//         <h1>{count} </h1>
-//         <button onClick={() => count(count == this.state.count + 1)}>
-//           Increase Count
-//         </button>
-//       </div>
-//     )
-//   }
-// }
-
-const App = () => {
-const [count, setCount] = useState(0)
-
-  return (
-<div>
-  <h1>{count}</h1>
-  <button onClick={() => setCount(count + 1)}>
-    Increase Count
-  </button>
-</div>
-  )
+class App extends React.Component {
+  // declaring state
+  state = {
+    count: 0,
+  }
+  render() {
+    // accessing the state value
+    const count = this.state.count
+    return (
+      <div className='App'>
+        <h1>{count} </h1>
+      <button onClick={() => this.setState({count: this.state.count + 1})}>
+          Increase Count
+        </button>
+      </div>
+    )
+  }
 }
+
 
 
 const rootElement = document.getElementById('root')
