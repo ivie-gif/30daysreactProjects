c557673af8c179078740eb5d2b381a399ea27342
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-08-04 22:53:55 +0100

    using state func comp increase count by two

--- src/index.js
+++ src/index.js
@@ -2,26 +2,20 @@
 import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component {
-  // declaring state
-  state = {
-    count: 0,
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  const handleAddCount = () => {
+    setCount(count + 2)
   }
-  render() {
-    // accessing the state value
-    const count = this.state.count
-    return (
-      <div className='App'>
-        <h1>{count} </h1>
-      <button onClick={() => this.setState({count: this.state.count + 1})}>
-          Increase Count
-        </button>
-        <button onClick={() => this.setState({count: this.state.count - 1})}>
-          Dcrease Count
-        </button>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={handleAddCount}> 
+        Increase count 
+      </button>
+    </div>
+  )
 }
 
 
