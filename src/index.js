90cd879e357a5dfbcf3d5fd088371a9ec85a724d
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-08-07 22:57:22 +0100

    using state func comp decrease count by three

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 // index.js
-import React, {useState} from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   const handleAddCount = () => {
-    setCount(count + 2)
-  }
+    setCount(count + 2);
+  };
+
+  const handleRemoveCount = () => {
+    setCount(count - 3);
+  };
+
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={handleAddCount}> 
-        Increase count 
-      </button>
+      <button onClick={handleAddCount}>Increase count + 2</button>
+      <button onClick={handleRemoveCount}>Decrease Count - 3</button>
     </div>
-  )
-}
+  );
+};
 
-
-
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
