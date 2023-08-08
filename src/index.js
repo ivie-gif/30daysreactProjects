bcc525bd284ae14d459a13f854a2fa47b5c46f16
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-08-08 22:59:51 +0100

    using state class comp plus and minus methods

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 // index.js
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+class App extends React.Component {
+  // declaring state
+  state = {
+    count: 0,
+  }
+  render() {
+    // accessing the state value
+    const count = this.state.count
+    return (
+      <div className='App'>
+        <h1>{count} </h1>
 
-const App = () => {
-  const [count, setCount] = useState(0);
-
-  const handleAddCount = () => {
-    setCount(count + 2);
-  };
-
-  const handleRemoveCount = () => {
-    setCount(count - 3);
-  };
-
-  return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={handleAddCount}>Increase count + 2</button>
-      <button onClick={handleRemoveCount}>Decrease Count - 3</button>
-    </div>
-  );
-};
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+        <div>
+          <button
+            onClick={() => this.setState({ count: this.state.count + 1 })}
+          >
+            Add One
+          </button>{' '}
+          <button
+            onClick={() => this.setState({ count: this.state.count - 1 })}
+          >
+            Minus One
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
