135730ab1328bbb61982c406d3b6a18668250d4c
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-08-14 14:30:32 +0100

    country flags and details

--- src/countList.js
+++ src/countList.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React from "react";
 
 // Component to be displayed
-const Countries = ({listOfCountry: {name, population, language, continent, image}}) => {
-    return (
-        <div style={{ margin: '0px 0px 40px 120px', padding: '15px 0px', color: 'grey'}}>
-           <img src={image} />
-           <h3>Name: {name} </h3>
-           <h3>Population: {population}</h3>  
-           <h3>Language: {language}</h3>
-           <h3>Continent: {continent}</h3>
-        </div>
-    )
-}
+const Countries = ({
+  listOfCountry: { name, population, language, continent, image },
+}) => {
+  return (
+    <div
+      style={{
+        margin: "0px 0px 40px 120px",
+        padding: "15px 0px",
+        color: "grey",
+      }}
+    >
+      <img src={image} />
+      <h3>Name: {name} </h3>
+      <h3>Population: {population}</h3>
+      <h3>Language: {language}</h3>
+      <h3>Continent: {continent}</h3>
+    </div>
+  );
+};
 
-// console.log(listOfCountry, '8uis7yhsayy')
-export const CountriesListItems = ({listOfCountries, btnState}) => {
-const listItems= listOfCountries[btnState]
-return <Countries listOfCountry={listItems} />
-}
+export const CountriesListItems = ({ listOfCountries, btnState }) => {
+  const listItems = listOfCountries[btnState];
+  return <Countries listOfCountry={listItems} />;
+};
--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import ReactDOM from "react-dom";
 import { CountriesListItems } from "../src/countList";
 
 const listOfCountries = [
-  // ["Nigeria", "30,000,000", "English", "Africa"],
-  // ["Ghana", "30,000", "English", "Africa"],
-  // ["South Africa", "100,000", "Zulu", "Africa"],
-  // ["Morocco", "300,000", "Arabic", "Africa"],
-  // ["Egypt", "400,000", "Arabic", "Africa"],
-  // ["Zambia", "100,000", "French", "Africa"]
-
   {
     name: "Nigeria",
     population: "300,000,000",
@@ -65,23 +58,21 @@ const App = () => {
   const [btnState, setBtnState] = useState(0);
 
   const handleBtnState = () => {
-    if (btnState == 2) {
+    if (btnState == 5) {
       setBtnState(0);
     } else {
       setBtnState(btnState + 1);
     }
   };
 
-  console.log(btnState, "2377yuqw");
-
   return (
     <div>
-      <h1>Countries and Flags</h1>
+      <h1 style={{textAlign: 'center', paddingTop: '80px'}}>Countries and Flags</h1>
       <div
         style={{
           marginLeft: "30%",
           marginRight: "30%",
-          boxShadow: "2px 2px 2px 2px grey",
+          boxShadow: "5px 5px 5px 5px grey",
         }}
       >
         <CountriesListItems
@@ -109,11 +100,3 @@ const App = () => {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-
-// ["https://th.bing.com/th/id/R.89f9b65feb2f0a7e5ce41a60c7e7e31f?rik=26RJtUW1YQqBcw&pid=ImgRaw&r=0",
-// "https://th.bing.com/th/id/OIP.PWgWDLoIO-cj2noL9bAPqgHaE7?w=246&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-// "https://th.bing.com/th/id/OIP.ZHRnBMS_Ce_t2XELz3sO6wHaEK?w=298&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-// "https://th.bing.com/th/id/OIP.02powjEKT0a_oeMozqi-2AHaE8?w=254&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-// "https://th.bing.com/th/id/OIP.4ve4zACsz1LZOlMcCUHGBAHaE8?w=263&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-// "https://th.bing.com/th/id/OIP.fy9DDB5uq8Vu-OzRyGrShgHaEK?w=291&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7"
-// ]
