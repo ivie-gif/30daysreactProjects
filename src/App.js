4d259796f04f6db22febfcd8bc40f7a3bdd04031
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-08-17 23:43:54 +0100

    folder structure

---
+++ src/App.js
@@ -0,0 +1,7 @@
+// src/App.js
+import React from 'react'
+const App = () => (<h1>Welcome to Checking 30 Days Of React</h1>)
+
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
--- src/buttonComp.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Button = ({onClick, text, style}) => (
-<button onClick={onClick} text={text} style={style}>
-    {text}
-</button>
-)
