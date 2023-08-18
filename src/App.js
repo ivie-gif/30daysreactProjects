3ffbcd7bed142933c330a124704c241351521631
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-08-18 09:41:15 +0100

    Event handling with alert

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-// src/App.js
 import React from 'react'
-const App = () => (<h1>Welcome to Checking 30 Days Of React</h1>)
-
-
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+// if it is functional components
+export const App = () => {
+  const greetPeople = () => {
+    alert('Welcome to 30 Days Of React Challenge')
+  }
+  return <button onClick={greetPeople}> Click Me </button>
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-// src/index.js
 // index.js
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { App } from './App'
 
-const App = () => <h1>Welcome to 30 Days Of React</h1>
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
+
 
