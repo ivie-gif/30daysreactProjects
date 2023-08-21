18c5a22eedc5fdaffdf96a179d75f9cb51210bf1
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-08-21 09:58:43 +0100

    anchor tag and alert event

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React, { Component } from "react";
-export class App extends Component {
-  greetPeople = () => {
-    alert("Welcome to 30 Days Of React Challenge");
+// import React, { Component } from 'react'
+// // if it is class components
+// export class App extends Component {
+//   handleClick = () => {
+//     alert('Welcome to 30 Days Of React Challenge')
+//   }
+//   render() {
+//     return (
+//       <a href='#' onClick={this.handleClick}>
+//         Click me
+//       </a>
+//     )
+//   }
+// }
+
+import React from "react";
+
+export const App = () => {
+  const handleClickLink = () => {
+    alert("Welcome to 30 days of React");
   };
 
-  render() {
-    return <button onClick={this.greetPeople}> Click Me </button>;
-  }
-}
+  return (
+    <a href="#" onClick={handleClickLink}>
+      Click this Link
+    </a>
+  );
+};
