bf0d587869739e9ebc4254f8f55991b75d462de4
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-08-21 09:49:00 +0100

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 export class App extends Component {
-     greetPeople = () => {
-         alert('Welcome to 30 Days Of React Challenge')
-       }
-    
-    render () {
-        return (
-            <button onClick={this.greetPeople}> Click Me </button> 
-      
-    )
-    }
+  greetPeople = () => {
+    alert("Welcome to 30 Days Of React Challenge");
+  };
+
+  render() {
+    return <button onClick={this.greetPeople}> Click Me </button>;
+  }
 }
