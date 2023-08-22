18eb7b02434c416f242adbc902240385e34efd4b
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-08-22 14:58:47 +0100

    field input validation suing func component

--- src/App.js
+++ src/App.js
@@ -1,84 +1,70 @@
-// index.js
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+// import ReactDOM from 'react-dom'
+
+// export class App extends Component {
+//   // declaring state
+//   // initial state
+//   state = {
+//     firstName: '',
+//   }
+//   handleChange = (e) => {
+//     const value = e.target.value
+//     this.setState({ firstName: value })
+//   }
+
+//   render() {
+//     /*
+//      accessing the state value and 
+//      this value will injected to the input in the value attribute
+//      */
+
+//     const firstName = this.state.firstName
+//     return (
+//       <div className='App'>
+//         <label htmlFor='firstName'>First Name: </label>
+//         <input
+//           type='text'
+//           id='firstName'
+//           name='firstName'
+//           placeholder='First Name'
+//           value={firstName}
+//           onChange={this.handleChange}
+//         />
+//         <h1>{this.state.firstName}</h1>
+//       </div>
+//     )
+//   }
+// }
+
+
+
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-export class App extends Component {
-  state = {
-    firstName: '',
-    message: '',
-    key: '',
-  }
-  handleClick = (e) => {
-    // e gives an event object
-    // check the value of e using console.log(e)
-    this.setState({
-      message: 'Welcome to the world of events',
-    })
-  }
-  // triggered whenever the mouse moves
-  handleMouseMove = (e) => {
-    this.setState({ message: 'mouse is moving' })
-  }
-  // to get value when an input field changes a value
-  handleChange = (e) => {
-    this.setState({
-      firstName: e.target.value,
-      message: e.target.value,
-    })
-  }
 
-  // to get keyboard key code when an input field is pressed
-  // it works with input and textarea
-  handleKeyPress = (e) => {
-    this.setState({
-      message:
-        `${e.target.value} has been pressed and the keycode is` + e.charCode,
-    })
-  }
-  // Blurring happens when a mouse leave an input field
-  handleBlur = (e) => {
-    this.setState({ message: 'Input field has been blurred' })
-  }
-  // This event triggers during a text copy
-  handleCopy = (e) => {
-    this.setState({
-      message: 'Using 30 Days Of React for commercial purpose is not allowed',
-    })
-  }
-  render() {
-    return (
-      <div>
-        <h1>Welcome to the World of Events</h1>
+export const App = () => {
+const [inputName, setInputName] = useState('')
 
-        <button onClick={this.handleClick}>Click Me</button>
-        <button onMouseMove={this.handleMouseMove}>Move mouse on me</button>
-        <p onCopy={this.handleCopy}>
-          Check copy right permission by copying this text
-        </p>
-
-        <p>{this.state.message}</p>
-        <label htmlFor=''> Test for onKeyPress Event: </label>
-        <input type='text' onKeyPress={this.handleKeyPress} />
-        <br />
-
-        <label htmlFor=''> Test for onBlur Event: </label>
-        <input type='text' onBlur={this.handleBlur} />
-
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label htmlFor='firstName'>First Name: </label>
-            <input
-              onChange={this.handleChange}
-              name='firstName'
-              value={this.state.value}
-            />
-          </div>
-
-          <div>
-            <input type='submit' value='Submit' />
-          </div>
-        </form>
-      </div>
-    )
-  }
+const handleNameValue = (e) => {
+    const value = e.target.value
+    setInputName(value)
 }
+
+console.log(inputName, '9877y')
+return (
+    <div>
+        <h3>First Name: </h3>
+        <input 
+        type='text'
+           id='firstName'
+           name='firstName'
+           placeholder='First Name'
+           value={inputName}
+           onChange={handleNameValue}
+        />
+
+        <h1>{inputName}</h1>
+    </div>
+)
+}
+
