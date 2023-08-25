f7bf70963355c8b9f610b9220ba1d8ef9a16fb9f
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-08-25 15:09:36 +0100

    field input validation using class component

--- src/App.js
+++ src/App.js
@@ -1,70 +1,37 @@
-// import React, { Component } from 'react'
-// import ReactDOM from 'react-dom'
-
-// export class App extends Component {
-//   // declaring state
-//   // initial state
-//   state = {
-//     firstName: '',
-//   }
-//   handleChange = (e) => {
-//     const value = e.target.value
-//     this.setState({ firstName: value })
-//   }
-
-//   render() {
-//     /*
-//      accessing the state value and 
-//      this value will injected to the input in the value attribute
-//      */
-
-//     const firstName = this.state.firstName
-//     return (
-//       <div className='App'>
-//         <label htmlFor='firstName'>First Name: </label>
-//         <input
-//           type='text'
-//           id='firstName'
-//           name='firstName'
-//           placeholder='First Name'
-//           value={firstName}
-//           onChange={this.handleChange}
-//         />
-//         <h1>{this.state.firstName}</h1>
-//       </div>
-//     )
-//   }
-// }
-
-
-
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-
-export const App = () => {
-const [inputName, setInputName] = useState('')
-
-const handleNameValue = (e) => {
+export class App extends Component {
+  // declaring state
+  // initial state
+  state = {
+    firstName: '',
+  }
+  handleChange = (e) => {
     const value = e.target.value
-    setInputName(value)
-}
+    this.setState({ firstName: value })
+  }
 
-console.log(inputName, '9877y')
-return (
-    <div>
-        <h3>First Name: </h3>
-        <input 
-        type='text'
-           id='firstName'
-           name='firstName'
-           placeholder='First Name'
-           value={inputName}
-           onChange={handleNameValue}
+  render() {
+    /*
+     accessing the state value and 
+     this value will injected to the input in the value attribute
+     */
+
+    const firstName = this.state.firstName
+    return (
+      <div className='App'>
+        <label htmlFor='firstName'>First Name: </label>
+        <input
+          type='text'
+          id='firstName'
+          name='firstName'
+          placeholder='First Name'
+          value={firstName}
+          onChange={this.handleChange}
         />
-
-        <h1>{inputName}</h1>
-    </div>
-)
+        <h1>{this.state.firstName}</h1>
+      </div>
+    )
+  }
 }
-
